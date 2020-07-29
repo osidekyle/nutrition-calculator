c5a66bae508a8847768c23b3332c108b72c2a483
osidekyle <hawkinskyle67@gmail.com>
2020-07-29 01:10:25 -0700

    initial commits, setup header component and some styling

---
+++ nutrition/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Header from "./components/Header"
+import './App.css';
+import bootstrap from "../../node_modules/bootstrap/dist/css/bootstrap.css"
+
+function App() {
+
+  
+
+  
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+      
+      <Header/>
+      
+      </div>
+
+    <div className="row">
+     
+
+    </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ nutrition/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import bootstrap from "../../../node_modules/bootstrap/dist/css/bootstrap.css"
+
+const Header = () => {
+
+    const headingStyle={
+        padding:"5%",
+        borderRadius:"25%",
+        boxShadow:"0px 5px 20px black",
+        width:"50%",
+        height:"75px",
+        margin:"auto",
+        paddingBottom:"9%",
+        background:"white"
+      }
+
+    const titleStyle={
+        textAlign:"center"
+    }
+
+    return (
+        <div className="heading mt-3 col-xs-12" style={headingStyle}>
+        <h1 className="Display1" style={titleStyle}> Nutrition Tracker</h1>
+        </div> 
+     );
+}
+ 
+export default Header;
