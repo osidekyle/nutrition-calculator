16f1d68423679992c30511cad75b388e73b46bcb
osidekyle <hawkinskyle67@gmail.com>
2020-08-05 10:31:26 -0700

    need to put header in container

--- nutrition/src/components/Header.js
+++ nutrition/src/components/Header.js
@@ -15,10 +15,12 @@ const Header = () => {
       }
 
     const titleStyle={
-        textAlign:"center"
+        textAlign:"center",
+        fontSize:"300%"
     }
 
     return (
+        //Put in container
         <div className="heading mt-3 col-xs-12" style={headingStyle}>
         <h1 className="Display1" style={titleStyle}> Nutrition Tracker</h1>
         </div> 
